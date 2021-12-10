7805ac1b0d5857262e0fe77e13dbf65e3f16aaa7
ukenia <80653250+ukenia@users.noreply.github.com>
2021-12-09 21:48:36 -0500

    Edited the streamlit app
    Added the files and modified app.py

--- app.py
+++ app.py
@@ -1,26 +1,57 @@
-import sys
+# import required libraries
 
-# # implement pip as a subprocess:
-# subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
-# 'matplotlib'])
-#import imageio
+import sys
 import streamlit as st
+import librosa
+import numpy as np
+
+# Basic page setup
 
 st.set_page_config(layout='centered')
 
-st.markdown("# MLSP Project")
+st.markdown("<p style='color:black;font-size:32px;text-align:center'><b>11755/18797 Machine Learning For Signal Processing: Final Project</b></p>", unsafe_allow_html=True)
+st.image("app_files/front_page.png")
 
-
-
-st.markdown("Lets listen to the reconstructed audio")
-file1='sample//'
-file2='out//'
+# create the singer to song dictionary
 
 singer_to_song = {'ADIZ':['01']}
 
-singer = st.sidebar.selectbox('Select a singer', ('ADIZ','VKOW'))
-select_dept = st.sidebar.selectbox('Select a song', singer_to_song[singer])
+# create the selection boxes
 
-aud1 = open(select_dept+"_recon.wav",'rb')
+singer = st.sidebar.selectbox('Select a singer', ('ADIZ','JLEE','JTAN','KENN','MCUR','MPOL','MPUR','NJAT','PMAR','SAMF','VKOW','ZHIY'))
+song = st.sidebar.selectbox('Select a song', singer_to_song[singer])
+
+# display the original song
+
+st.markdown("Lets listen to the original song first")
+aud1 = open("outputs/original/"+str(song)+"_"+str(singer)+".wav",'rb')
 aud1 = aud1.read()
 st.audio(aud1)
+
+# display the lyrics
+
+st.markdown("Lets listen to the lyrics now")
+aud2 = open("outputs/read/"+str(song)+"_"+str(singer)+".wav",'rb')
+aud2 = aud2.read()
+st.audio(aud2)
+
+# display the reconstructed song
+
+st.markdown("Lets listen to the reconstructed audio")
+aud3 = open("outputs/reconstructed/"+str(song)+"_"+str(singer)+".wav",'rb')
+aud3 = aud3.read()
+st.audio(aud3)
+
+# display the similarity metric
+
+y1, sr1 = librosa.load("outputs/original/"+str(song)+"_"+str(singer)+".wav")
+spectrogram1 = librosa.stft(y1, n_fft=1024, hop_length=512, center=False, win_length=1024)
+M1 = abs(spectrogram1)
+
+y2, sr2 = librosa.load("outputs/reconstructed/"+str(song)+"_"+str(singer)+".wav")
+spectrogram2 = librosa.stft(y2, n_fft=1024, hop_length=512, center=False, win_length=1024)
+M2 = abs(spectrogram2)
+
+sim_metric = (np.linalg.norm(np.dot(M2.T,M1))/np.linalg.norm(M2)/np.linalg.norm(M1))/(np.linalg.norm(np.dot(M2.T,M2))/np.linalg.norm(M2)/np.linalg.norm(M2))
+
+st.metric('Similarity', str(round(sim_metric,2)))
